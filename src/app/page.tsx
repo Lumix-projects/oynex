4cbe21c2f59eaf6479051ae0a47b3d86ccccdc49
ahmed-abdelmottal3 <abdelmottale3@gmail.com>
2025-10-29 20:31:54 +0300

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import { Button } from "@/components/ui/button";
+import Hero from "@/components/shared/Hero";
 
 export default function Home() {
-  return <Button variant={"outline"}>Button</Button>;
+  return (
+    <>
+      <Hero 
+      title="Revolutionizing Skincare Through Dermatological Science"
+      subtitle="Experience the pinnacle of dermatological innovation with our scienfically-formulated skincare products, designed to deliver visible results and promote lasting skincare"
+      img="/Hero.webp"
+      button={true}
+      />
+      Test
+    </>
+  )
 }
---
+++ src/components/shared/Hero.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import { Button } from "../ui/button"
+type HeroProps = {
+  button?: boolean
+  title: string
+  subtitle: string
+  img: string
+}
+export default function Hero({title, subtitle,img, button=false}:HeroProps) {
+  return (
+    <section className="relative w-full h-screen">
+      <Image
+        src={img}
+        alt="Hero Image"
+        fill
+        className="object-cover object-center"
+        priority
+      />
+
+      <div className="absolute inset-0 flex flex-col gap-5 items-center justify-center text-center bg-foreground/50 px-4">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold max-w-6xl text-white leading-tight drop-shadow-lg">
+          {title}
+        </h1>
+        <p className="text-base sm:text-lg md:text-xl text-background/60 max-w-2xl drop-shadow">
+          {subtitle}
+        </p>
+
+        {button && (
+            <Button className="bg-main text-background">Explore Our Products</Button>
+        )}
+      </div>
+    </section>
+  )
+}
