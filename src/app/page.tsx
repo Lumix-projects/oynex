90f43cc513818781d3383dbbaafc153afbb35068
ahmed-abdelmottal3 <abdelmottale3@gmail.com>
2025-10-30 02:15:45 +0300

    add innovations section, create productcard componnent and create headings componnent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import Hero from "@/components/shared/Hero";
+import Headings from "@/components/ui/headings";
+import ProductCard from "@/components/ui/prodcutcard";
 
 export default function Home() {
   return (
     <>
-      <Hero 
-      title="Revolutionizing Skincare Through Dermatological Science"
-      subtitle="Experience the pinnacle of dermatological innovation with our scienfically-formulated skincare products, designed to deliver visible results and promote lasting skincare"
-      img="/Hero1.jpg"
-      button={true}
+      {/* hero section */}
+      <Hero
+        title="Revolutionizing Skincare Through Dermatological Science"
+        subtitle="Experience the pinnacle of dermatological innovation with our scienfically-formulated skincare products, designed to deliver visible results and promote lasting skincare"
+        img="/Hero1.jpg"
+        button={true}
       />
+
+      {/* innovations */}
+      <section className="bg-foreground/10 py-20">
+      <Headings title="Discover Our Innovations" subtitle="Redefining beauty through precision and innovation." />
+      <ProductCard />
+      </section>
     </>
   )
 }
---
+++ src/components/ui/headings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Headings({title, subtitle}:{title:string, subtitle:string}) {
+  return (
+      <div className="flex flex-col items-center justify-center mb-10">
+        <h2 className="text-3xl font-semibold">{title}</h2>
+        <h5 className="text-sm text-foreground/40">{subtitle}</h5>
+      </div>
+        )
+}
+
+export default Headings
